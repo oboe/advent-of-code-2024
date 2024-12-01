3534abf1b80754ee1b3bec8c609b7022445e3ee9
oboe <155313342+oboe@users.noreply.github.com>
2024-12-01 10:10:35 +0000

    Update paths

--- build-and-run.sh
+++ build-and-run.sh
@@ -5,7 +5,7 @@ day=$1
 
 /usr/bin/clang++ -fcolor-diagnostics -fansi-escape-codes -g -pedantic-errors -Wall -Weffc++ \
 -Wextra -Wconversion -Wsign-conversion -Werror -std=c++20 \
-/Users/shintaroonuma/workplace/advent-of-code/$day/**.cpp \
--o /Users/shintaroonuma/workplace/advent-of-code/$day/main \
+$day/**.cpp \
+-o $day/main \
 
-/Users/shintaroonuma/workplace/advent-of-code/$day/main
+$day/main
