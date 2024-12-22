f25d1726c86b37ddc66f928d286da5dd5adcf423
oboe <155313342+oboe@users.noreply.github.com>
2024-12-22 12:06:45 +0000

    Setup clang-formatting

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,6 +3,8 @@ set -eux
 
 day=$1
 
+clang-format -i -style=Google $day/**.cpp #$day/**.h 
+
 /usr/bin/clang++ -O3 -fcolor-diagnostics -fansi-escape-codes -g -pedantic-errors -Wall -Weffc++ \
 -Wextra -Wconversion -Wsign-conversion -Werror -std=c++20 \
 $day/**.cpp \
